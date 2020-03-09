2317114c0476a7bbd2177506b57b9e6067d194f0
James Hagood <cardfaux@tutanota.com>
2020-03-09 17:44:37 -0500

    added Event File

--- Client/src/Components/Calendar/Calendar.jsx
+++ Client/src/Components/Calendar/Calendar.jsx
@@ -1,54 +1,58 @@
-import React, { Component } from "react";
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import { v1 as uuidv1 } from 'uuid';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
-import "../../App.css";
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import '../../App.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
-const DnDCalendar = withDragAndDrop(Calendar);
+const DragAndDropCalendar = withDragAndDrop(Calendar);
 
-class App extends Component {
-  state = {
+const MainCalendar = (props) => {
+  const [state, setState] = useState({
     events: [
       {
+        id: uuidv1(),
         start: new Date(),
-        end: new Date(moment().add(2, "days")),
-        title: "Dr. Appointment"
+        end: new Date(moment().add(2, 'days')),
+        title: 'Dr. Appointment'
       }
     ]
-  };
+  });
 
-  onEventResize = (type, { event, start, end, allDay }) => {
-    this.setState(state => {
+  const onEventResize = (type, { event, start, end, allDay }) => {
+    setState((state) => {
       state.events[0].start = start;
       state.events[0].end = end;
       return { events: state.events };
     });
   };
 
-  onEventDrop = ({ event, start, end, allDay }) => {
+  const onEventDrop = ({ event, start, end, allDay }) => {
     console.log(start);
+    console.log(end);
+    console.log(event);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <DnDCalendar
-          defaultDate={new Date()}
-          defaultView="month"
-          events={this.state.events}
-          localizer={localizer}
-          onEventDrop={this.onEventDrop}
-          onEventResize={this.onEventResize}
-          resizable
-          style={{ height: "100vh" }}
-        />
-      </div>
-    );
-  }
-}
+  console.log(state.events[0]);
 
-export default App;
+  return (
+    <div className='App'>
+      <DragAndDropCalendar
+        defaultDate={new Date()}
+        defaultView='month'
+        events={state.events}
+        localizer={localizer}
+        onEventDrop={onEventDrop}
+        onEventResize={onEventResize}
+        resizable
+        style={{ height: '100vh' }}
+      />
+    </div>
+  );
+};
+
+export default MainCalendar;
