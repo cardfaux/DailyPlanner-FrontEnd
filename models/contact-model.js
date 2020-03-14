a9ba101e931c94da0133ab3fb0e2a4c734e752e1
James Hagood <cardfaux@tutanota.com>
2020-03-14 10:44:02 -0500

    added contact Controllers for all CRUD events

---
+++ models/contact-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+	name: { type: String, required: true },
+	email: { type: String, required: true },
+	address: { type: String },
+	birthday: { type: Date },
+	anniversary: { type: Date },
+	creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
+});
+
+module.exports = mongoose.model('Contact', contactSchema);
--- routes/contacts-routes.js
+++ routes/contacts-routes.js
@@ -9,4 +9,36 @@ const router = express.Router();
 
 router.get('/', contactsControllers.getContacts);
 
+router.use(checkAuth);
+
+router.get('/me', contactsControllers.getMyContacts);
+
+router.post(
+	'/',
+	[
+		check('name')
+			.not()
+			.isEmpty(),
+		check('email')
+			.normalizeEmail()
+			.isEmail()
+	],
+	contactsControllers.createMyContacts
+);
+
+router.patch(
+	'/:cid',
+	[
+		check('name')
+			.not()
+			.isEmpty(),
+		check('email')
+			.normalizeEmail()
+			.isEmail()
+	],
+	contactsControllers.updateContactById
+);
+
+router.delete('/:cid', contactsControllers.deleteContactById);
+
 module.exports = router;
