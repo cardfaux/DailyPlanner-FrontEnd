3f848bdaddc675ceeb17cdbe73c1434cebf08c63
James Hagood <cardfaux@tutanota.com>
2020-03-15 18:57:05 -0500

    added myNotes Card

---
+++ Client/src/Components/HomePage/MyNotesCard/MyNotesCard.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useContext } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { FaEvernote } from 'react-icons/fa';
+
+import ErrorModal from '../../../Shared/Components/UIElements/ErrorModal/ErrorModal';
+import LoadingSpinner from '../../../Shared/Components/UIElements/LoadingSpinner/LoadingSpinner';
+import Card from '../../../Shared/Components/UIElements/Card/Card';
+import { useHttpClient } from '../../../Shared/Hooks/Http-Hook';
+import { AuthContext } from '../../../Shared/Context/auth-context';
+
+import { Secondary, Primary } from '../../../Styles/JS/Colors';
+
+const MyNotesCard = ({ className }) => {
+  const auth = useContext(AuthContext);
+  const [noteCount, setNoteCount] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const responseData = await sendRequest(
+          `${process.env.REACT_APP_BACKEND_URL}/notes/me`,
+          'GET',
+          null,
+          {
+            Authorization: 'Bearer ' + auth.token
+          }
+        );
+        setNoteCount(responseData.notes.length);
+      } catch (err) {}
+    };
+    fetchNotes();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && (
+        <div className={className}>
+          <Link to='/notes'>
+            <Card>
+              <h1>
+                <FaEvernote /> My Notes: {noteCount}
+              </h1>
+            </Card>
+          </Link>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default styled(MyNotesCard)`
+  margin: 4rem;
+  width: 30rem;
+  text-align: center;
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+  div {
+    &:hover {
+      background: ${Secondary};
+      color: ${Primary};
+    }
+  }
+  svg {
+    color: ${Primary};
+  }
+`;
--- Client/src/Pages/HomePage/HomePage.jsx
+++ Client/src/Pages/HomePage/HomePage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import MyEventsCard from '../../Components/HomePage/MyEventsCard/MyEventsCard';
+import MyNotesCard from '../../Components/HomePage/MyNotesCard/MyNotesCard';
 
 const HomePage = (props) => {
   return (
     <div className={props.className}>
       <MyEventsCard />
-      <MyEventsCard />
+      <MyNotesCard />
     </div>
   );
 };
